1ce89f75c7b34bd60b1f839f05c0e944f310fe86
cdcurry <cdcurry@krispoint.com>
2017-11-27 15:08:40 +0100

    Update to latest dmr-test-system certificate (prev. was expired)

--- build-client-truststore.sh
+++ build-client-truststore.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-CLIENT_TUSTSTORE=src/main/resources/keystore/client-truststore.jks
+CLIENT_TRUSTSTORE=src/main/resources/keystore/client-truststore.jks
 
 echo "Before import ..."
-keytool -list -keystore $CLIENT_TUSTSTORE -storepass storepassword
+keytool -list -keystore $CLIENT_TRUSTSTORE -storepass storepassword
 
 echo "Importing ..."
 
 # Trust certificate used by server to sign response.
-keytool -noprompt -import -alias skatserver -file pem/dmr-test-system.pem -keystore $CLIENT_TUSTSTORE -storepass storepassword
+keytool -noprompt -import -alias skatserver -file pem/dmr-test-system.pem -keystore $CLIENT_TRUSTSTORE -storepass storepassword
 
 # Import SSL cert for secure transport (https://..)
 # The full cert. chain is viewable with openssl command: openssl s_client -connect host:port -showcerts
 #
-keytool -noprompt -import -alias ssl_chain_0 -file pem/ssl-chain-0.pem -keystore $CLIENT_TUSTSTORE -storepass storepassword
+keytool -noprompt -import -alias ssl_chain_0 -file pem/ssl-chain-0.pem -keystore $CLIENT_TRUSTSTORE -storepass storepassword
 # Level 0 is sufficient for completeness we import the full chain (level 1,2,3)...
-keytool -noprompt -import -alias ssl_chain_1 -file pem/ssl-chain-1.pem -keystore $CLIENT_TUSTSTORE -storepass storepassword
-keytool -noprompt -import -alias ssl_chain_2 -file pem/ssl-chain-2.pem -keystore $CLIENT_TUSTSTORE -storepass storepassword
+keytool -noprompt -import -alias ssl_chain_1 -file pem/ssl-chain-1.pem -keystore $CLIENT_TRUSTSTORE -storepass storepassword
+keytool -noprompt -import -alias ssl_chain_2 -file pem/ssl-chain-2.pem -keystore $CLIENT_TRUSTSTORE -storepass storepassword
 
 echo "After import ..."
-keytool -list -keystore $CLIENT_TUSTSTORE -storepass storepassword
+keytool -list -keystore $CLIENT_TRUSTSTORE -storepass storepassword
 
 ### Reverse imports
 ### NOT ACTIVE
